6440cd31c20ea6b0752d6ad8c363b0f1833c2fbc
HOME\Користувач <yulia.furta@gmail.com>
2017-04-25 23:20:35 +0300

    Initial commit

---
+++ MenuOnWebFinal/MenuOnWebFinal/Controllers/AdminController.cs
@@ -0,0 +1,84 @@
+﻿using MenuOnWebFinal.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MenuOnWebFinal.Controllers
+{
+    [Authorize(Roles ="Administrator")]
+    public class AdminController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+      
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(FormCollection form)
+        {
+            var userManager = new UserManager<User>(new UserStore<User>(context));
+            string userName = form["txtEmail"];
+
+            string email = form["txtEmail"];
+            string pwd = form["txtPassword"];
+            string role = form["txtRole"];
+
+            var user = new User();
+            user.UserName = userName;
+            user.Email = email;
+
+            var newUser = userManager.Create(user, pwd);
+
+            if (newUser.Succeeded)
+            {
+              var result = userManager.AddToRole(user.Id, role);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult AssignRole()
+        {
+            ViewBag.Roles = context.Roles.Select(r => new SelectListItem { Value = r.Name, Text = r.Name }).ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AssignRole(FormCollection form)
+        {
+            string userName = form["txtUserName"];
+            string role = form["RoleName"];
+
+            User existingUser = context.Users.Where(u => u.UserName.Equals(userName)).FirstOrDefault();
+            var userManager = new UserManager<User>(new UserStore<User>(context));
+            userManager.AddToRole(existingUser.Id, role);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult DeleteUser()
+        {
+            ViewBag.Users = context.Users.Select(r => new SelectListItem { Value = r.UserName, Text = r.UserName }).ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteUser(FormCollection form)
+        {
+            string userName = form["Users"];
+
+            var userManager = new UserManager<User>(new UserStore<User>(context));
+            var user=userManager.FindByEmail(userName);
+            var usersRoles = userManager.GetRoles(user.Id);
+
+            foreach (var item in usersRoles)
+            {
+                userManager.RemoveFromRole(user.Id,item);
+            }
+            userManager.Delete(user);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ MenuOnWebFinal/MenuOnWebFinal/Controllers/RecipeController.cs
@@ -0,0 +1,239 @@
+﻿using MenuOnWebFinal.Models;
+using System;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace MenuOnWebFinal.Controllers
+{
+    public class RecipeController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Recipe/Details/5
+        public ActionResult Details(int id)
+        {
+            var recipe = db.Recipes.FirstOrDefault(i => i.Id == id);
+            if (recipe == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Comments = db.Comments.Where(i => i.RecipeId == id);
+            return View((ViewRecipe)recipe);
+        }
+
+        // GET: Recipe/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(RecipeAddModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Recipe recipe = new Recipe
+                {
+                    Name = model.Name,
+                    Text = model.Text,
+                    CreateDate = DateTime.Now,
+                    Tags = model.IngridietsString,
+                    UserId = User.Identity.GetUserId(),
+                };
+                string imagePath = UploadFile(model.ImageFile, "images/thumbnails");
+                recipe.ImageUrl = imagePath;
+
+                db.Recipes.Add(recipe);
+                db.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public string UploadFile(HttpPostedFileBase file, string pathPart)
+        {
+            if (file == null) return string.Empty;
+
+            var fileName = Path.GetFileName(file.FileName);
+            var random = Guid.NewGuid() + fileName;
+            var path = Path.Combine(HttpContext.Server.MapPath("~/Content/" + pathPart), random);
+            if (!Directory.Exists(HttpContext.Server.MapPath("~/Content/" + pathPart)))
+            {
+                Directory.CreateDirectory(HttpContext.Server.MapPath("~/Content/" + pathPart));
+            }
+            file.SaveAs(path);
+
+            return random;
+        }
+
+        // GET: Recipe/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var recipe = db.Recipes.Find(id);
+            if (recipe == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(new RecipeAddModel
+            {
+                Id = recipe.Id,
+                Name = recipe.Name,
+                Text = recipe.Text,
+                UserId = User.Identity.GetUserId(),
+                IngridietsString = recipe.Tags,
+                ImageFile = null
+            });
+        }
+
+        // POST: Recipe/Edit/5
+        [HttpPost]
+        public ActionResult Edit(RecipeAddModel editedRecipe)
+        {
+            try
+            {
+                if (editedRecipe.IngridietsString == null)
+                {
+                    throw new Exception();
+                }
+
+                var recipeToUpdate = db.Recipes.Find(editedRecipe.Id);
+
+                string imagePath = "";
+                string imageToDelete = "";
+                if (editedRecipe.ImageFile != null)
+                {
+                    imagePath = UploadFile(editedRecipe.ImageFile, "images/thumbnails");
+                    imageToDelete = recipeToUpdate.ImageUrl;
+                }
+
+
+                recipeToUpdate.Name = editedRecipe.Name;
+                recipeToUpdate.Text = editedRecipe.Text;
+                recipeToUpdate.Tags = editedRecipe.IngridietsString;
+                if (imagePath != "")
+                {
+                    recipeToUpdate.ImageUrl = imagePath;
+                }
+
+                db.Entry(recipeToUpdate).State = EntityState.Modified;
+                db.SaveChanges();
+
+                if (imageToDelete != "")
+                {
+                    System.IO.File.Delete(HttpContext.Server.MapPath("~/Content/images/thumbnails/" + imageToDelete));
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Recipe/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var recipeToDelite = db.Recipes.Find(id);
+            if (recipeToDelite == null)
+            {
+                return HttpNotFound();
+            }
+            if (recipeToDelite.ImageUrl != null)
+            {
+                try
+                {
+                    System.IO.File.Delete(HttpContext.Server.MapPath("~/Content/images/thumbnails/" + recipeToDelite.ImageUrl));
+                }
+                catch { }
+            }
+
+            db.Recipes.Remove(recipeToDelite);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(CommentModel comment)
+        {
+            try
+            {
+                var commentToAdd = (Comment)comment;
+                commentToAdd.UserId = User.Identity.GetUserId();
+                db.Comments.Add(commentToAdd);
+                db.SaveChanges();
+                return RedirectToAction("Details", "Recipe", new { id = comment.RecipeId });
+            }
+            catch
+            {
+                return RedirectToAction("Details", "Recipe", new { id = comment.RecipeId });
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeleteComment(int id)
+        {
+            try
+            {
+                var commentToDelete = db.Comments.Find(id);
+                if (commentToDelete == null)
+                {
+                    throw new Exception();
+                }
+
+                db.Comments.Remove(commentToDelete);
+                db.SaveChanges();
+                return RedirectToAction("Details", "Recipe", new { id = commentToDelete.RecipeId });
+            }
+            catch
+            {
+                return HttpNotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Like(LikeModel like)
+        {
+            var l = db.Likes.FirstOrDefault(i => i.UserId == like.UserId && i.RecipeId == like.RecipeId);
+            if (l != null)
+            {
+                if (l.Value == 0)
+                    l.Value = 1;
+                else
+                    l.Value = 0;
+
+                var val = l.Value;
+
+                db.Entry(l).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+
+
+                return RedirectToAction("Details", "Recipe", new { id = like.RecipeId });
+            }
+            else
+            {
+                var likeToAdd = new Like()
+                {
+                    UserId = like.UserId,
+                    RecipeId = like.RecipeId,
+                    Value = 1
+                };
+                db.Likes.Add(likeToAdd);
+
+                await db.SaveChangesAsync();
+
+                return RedirectToAction("Details", "Recipe", new { id = like.RecipeId });
+            }
+        }
+    }
+}
